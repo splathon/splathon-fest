0b7736f1c3e182e80b9e31b6df108e2e5769818a
Hirokazu Nishioka <hiro@nisshiee.org>
2016-01-09 12:05:25 +0900

    Show players on Fest#show

--- app/models/fest.rb
+++ app/models/fest.rb
@@ -1,5 +1,7 @@
 class Fest < ApplicationRecord
   has_many :themes
+  has_many :players
+
   validates :name, presence: true, length: { maximum: 255 }
   enum status: { in_preparation: 0, opened: 1 }
 
