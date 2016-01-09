1bf643d43bdd07cb9b9ebcb7996a3e5a690645a9
Hirokazu Nishioka <hiro@nisshiee.org>
2016-01-10 00:55:56 +0900

    Fix bug

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module SplathonFest
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.force_ssl = false
   end
 end
